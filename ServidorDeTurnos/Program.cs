8bf6d68cb135fca9dc83d2558d62bace1c7d71c4
Alejandro <alejandromirandaco1801@gmail.com>
2025-02-05 11:59:15 +0100

    Agregar archivos de proyecto.

---
+++ ServidorDeTurnos/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServidorDeTurnos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ShiftServer server = new ShiftServer();
+            server.Init();
+
+            Console.WriteLine(server.users.Length);
+
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ ServidorDeTurnos/ShiftServer.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServidorDeTurnos
+{
+    internal class ShiftServer
+    {
+        public string[] users;
+        public List<string> waitQueue; //Tambien estan esperando por silksong Ç_Ç  
+
+
+
+        public void ReadNames(string route)
+        {
+            try
+            {
+                using (StreamReader sr = new StreamReader(route))
+                {
+                    string list = sr.ReadToEnd();
+                    string[] listedNames = list.Split(';');
+
+                    users = new string[listedNames.Length];
+
+                    for (int i = 0; i < users.Length; i++)
+                    {
+                        users[i] = listedNames[i];
+                    }
+                }
+            }
+            catch (Exception ex) when (ex is IOException || ex is ArgumentException)
+            {
+            }
+        }
+
+
+        public int ReadPin(string route)
+        {
+            try
+            {
+                using (BinaryReader br = new BinaryReader(new FileStream(route, FileMode.Open)))
+                {
+                    return br.ReadInt32();
+                }
+            }
+            catch (Exception ex) when (ex is IOException || ex is ObjectDisposedException)
+            {
+                return -1;
+            }
+        }
+
+        public void Init()
+        {
+            ReadNames("C:\\Users\\Alejandro\\Desktop\\usuarios.txt");
+        }
+    }
+}
